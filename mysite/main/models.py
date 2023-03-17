80b44f5c281f7fbeeb3c92d1b4b60b6596bc36ca
RRoxxxsii <mishabur38@yandex.com>
2023-03-17 12:02:05 +0300

    Initial commit

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -5,22 +5,31 @@ from django.db import models
 class Car(models.Model):
     model_name = models.CharField(max_length=50)
     price = models.PositiveIntegerField()
-    brand = models.ForeignKey('CarBrand', on_delete=models.CASCADE)
+    brand = models.ForeignKey('Brand', on_delete=models.CASCADE)
     body = models.ForeignKey('Body', on_delete=models.CASCADE)
     drive = models.ForeignKey('Drive', on_delete=models.CASCADE)
     transmission = models.ForeignKey('Transmission', on_delete=models.CASCADE)
     color = models.ForeignKey('Color', on_delete=models.CASCADE)
+    fuel = models.ForeignKey('FuelType', on_delete=models.CASCADE)
+    wheel = models.ForeignKey('SteeringWheel', on_delete=models.CASCADE)
+    engine = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.model_name
+
+    class Meta:
+        verbose_name_plural = 'Автомобили'
 
 
 # Базовые таблицы
-class CarBrand(models.Model):
+class Brand(models.Model):
     brand_name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.brand_name
 
     class Meta:
-        verbose_name_plural = 'Car Brands'
+        verbose_name_plural = 'Марки'
 
 
 class Body(models.Model):
@@ -30,7 +39,7 @@ class Body(models.Model):
         return self.body_name
 
     class Meta:
-        verbose_name_plural = 'Bodies'
+        verbose_name_plural = 'Кузовы'
 
 
 class Drive(models.Model):
@@ -40,7 +49,7 @@ class Drive(models.Model):
         return self.drive_name
 
     class Meta:
-        verbose_name_plural = 'Drives'
+        verbose_name_plural = 'Приводы'
 
 
 class Transmission(models.Model):
@@ -50,7 +59,7 @@ class Transmission(models.Model):
         return self.transmission_name
 
     class Meta:
-        verbose_name_plural = 'Transmissions'
+        verbose_name_plural = 'Трансмиссии'
 
 
 class Color(models.Model):
@@ -60,6 +69,24 @@ class Color(models.Model):
         return self.color_name
 
     class Meta:
-        verbose_name_plural = 'Colors'
+        verbose_name_plural = 'Цвета'
 
 
+class SteeringWheel(models.Model):
+    wheel = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.wheel
+
+    class Meta:
+        verbose_name = 'Руль'
+
+class FuelType(models.Model):
+    fuel = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.fuel
+
+    class Meta:
+        verbose_name = 'Топливо'
+        verbose_name_plural = 'Топливо'
